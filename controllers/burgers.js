b180b83fb96a188465eda6b5b34a88b05dcf4103
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2019-12-20 18:09:38 -0500

    finished routes on for API in controller tested all in curl

--- controllers/burgers.js
+++ controllers/burgers.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Burgers = require('../models/burgers.js');
 
+//This is the read route
 router.get('/', (req, res)=> {
-  res.send('index');
+  Burgers.find({}, (err, foundBurgers)=>{
+    res.json(foundBurgers);
+  });
 });
+
+//This is the create route
+router.post('/', (req,res)=> {
+  Burgers.create(req.body, (err, createdBurger)=> {
+      res.json(createdBurger);
+  });
+});
+
+
+//This is the delete route
+router.delete('/:id', (req, res)=> {
+  Burgers.findByIdAndRemove(req.params.id, (err, deletedBurger)=>{
+     res.json(deletedBurger)
+  });
+});
+
+//This is the update route
+router.put('/:id', (req, res)=> {
+  Burgers.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedBurger)=> {
+    res.json(updatedBurger);
+  })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+app.use(express.json());
 
 const burgersController = require('./controllers/burgers.js');
 app.use('/burgers', burgersController);
