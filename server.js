e31c959e5228074cf9a11fd17e7261585b4365f6
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2019-12-20 19:45:34 -0500

    added index.html and app.js file

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
 app.use(express.json());
+app.use(express.static('public'));
+
 
 const burgersController = require('./controllers/burgers.js');
 app.use('/burgers', burgersController);
