76284bc7745c4c678e62a46ab309f33624da2ec1
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2020-01-03 17:21:17 -0500

    setting up heroku and mongo atlas configuration

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+//dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const app = express();
+const db = mongoose.connection;
+const session = require('express-session');
+require('dotenv').config();
 
+// PORT - This allows the use of Herokus port or my local port depending on the
+// environment
+const PORT = process.env.PORT
+
+//Database - Connect to db either through heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+console.log(MONGODB_URI);
+
+//This connects to Mongo and fixes deprication warnings from Mongoose -
+//may or may not need them based on version of Mongoose
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex:true})
+
+//MIDDLEWARE
+
+//This allows you to use public folder for static assets
+app.use(express.static('public'));
 
 app.use(express.json());
-app.use(express.static('public'));
+
 app.use(session({
   secret:'feedmeseymour',
   resave:false,
@@ -27,7 +46,8 @@ mongoose.connection.once('open', () => {
     console.log('connected to mongoose...');
 });
 
+app.listen(PORT, () => console.log('Listening on port:', PORT));
 
-app.listen(3000, ()=>{
-    console.log('listening...');
-});
+// app.listen(3000, ()=>{
+//     console.log('listening...');
+// });
